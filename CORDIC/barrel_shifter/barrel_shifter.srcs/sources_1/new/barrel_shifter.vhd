3ab4f783f997d0e394c280608cfb5c9fb99813d9
Shuhangtan <51217607+Shuhangtan@users.noreply.github.com>
2021-05-19 22:57:03 -0700

    Finish(?) barrel_shifter.vdl

--- CORDIC/barrel_shifter/barrel_shifter.srcs/sources_1/new/barrel_shifter.vhd
+++ CORDIC/barrel_shifter/barrel_shifter.srcs/sources_1/new/barrel_shifter.vhd
@@ -27,13 +27,48 @@ entity barrel_shifter is
 end barrel_shifter;
 
 architecture Behavioral of barrel_shifter is
+
+signal data, shift8, shift4, shift2, shift1: unsigned (15 downto 0):=(others => '0');
+
 begin
 
 -- Register process
 p1:process (rst, clk)
   begin 
-    if
-    end if;       
-  end process;
+    if rst = '1' then
+	  d_out <= "0000000000000000";
+	elsif rising_edge(clk) then
+	  d_out <= shift1;
+	end if;    
+  end process p1;
+  
+-- Shifting process
+p2:process (shift_by)
+  begin
+    -- shift 8 bits
+	if (shift_by(3) = '1') then
+	  shift8 <= "00000000" & data(15 downto 8);
+	else
+	  shift8 <= data;
+	end if;
+	-- shift 4 bits
+	if (shift_by(2) = '1') then
+	  shift4 <= "0000" & data(15 downto 4);
+	else
+	  shift4 <= shift8;
+	end if; 
+	-- shift 2 bits
+	if (shift_by(1) = '1') then
+	  shift2 <= "00" & data(15 downto 2);
+	else
+	  shift2 <= shift4;
+	end if;
+	-- shift 1 bit
+	if (shift_by(0) = '1') then
+	  shift1 <= "0" & data(15 downto 1);
+	else
+	  shift1 <= shift2;
+	end if;
+  end process p2;
 
 end Behavioral;
